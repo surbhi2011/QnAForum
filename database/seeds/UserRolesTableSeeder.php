2c5e0a36208b2b5578f10bd32b6f6a6e37f2b705
Dveep Chaudhari <dveepchaudhari@gmail.com>
2019-01-21 19:27:57 +0530

    seeder

---
+++ database/seeds/UserRolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+class UserRolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=DB::table('users')->latest()->first()->id();
+        $role=DB::table('roles')->where('name','SuperAdmin')->pluck('id');
+        //$role=Role::where('name' , 'SuperAdmin')->get();
+        DB::table('user_roles')->insert([
+           'role_id'=> $role,
+            'user_id'=> $user,
+        ]);
+    }
+}
